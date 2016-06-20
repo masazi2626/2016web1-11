155ce074a9454ef1cd799f4382233779b0d94c1d
masazi2626 <luigimark2@i.softbank.jp>
2016-06-20 17:16:17 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,8 +1,7 @@
+while(){
 
 var number = Math.floor(Math.random()*5);
 
-while()
-{
 var answer = parseInt(window.prompt('数当てゲーム。0~4の数字を入力してください！'));
 
 var message;
@@ -15,5 +14,6 @@ if(answer == number){
 }else{
   message = '0~4の数字を入力してください';
 }
-}
+
 document.getElementById('choice').textContent = message;
+}
