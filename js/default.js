18eacf1709b1e0dfe7204dc6d9f3316e66dc84c9
masazi2626 <luigimark2@i.softbank.jp>
2016-06-20 17:34:29 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,22 +1,18 @@
 var number = Math.floor(Math.random()*3);
-var i=0;
-while(i<10)
-{
-var answer = parseInt(window.prompt('数当てゲーム。0~2の数字を入力してください！'));
 
+
+var answer = parseInt(window.prompt('数当てゲーム。0~2の数字を入力してください！'));
+while(answer == number)
+{
 var message;
 if(answer == number){
   message = 'あたり！';
-  break;
 }else if(answer < number){
   message = '残念！もっと大きいです';
-  i+=1;
 }else if(number < answer){
   message = '残念！もっと小さいです';
-  i+=1;
 }else{
   message = '0~2の数字を入力してください';
-  i+=1;
 }
 
 
