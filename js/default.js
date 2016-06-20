074ec9e7ee7cad081a98db6789ed66ed2fabd0e8
masazi2626 <luigimark2@i.softbank.jp>
2016-06-20 17:23:31 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -5,15 +5,12 @@ var answer = parseInt(window.prompt('数当てゲーム。0~2の数字を入力�
 var message;
 if(answer == number){
   message = 'あたり！';
-}
-do{
-if(answer < number){
+}else if(answer < number){
   message = '残念！もっと大きいです';
 }else if(number < answer){
   message = '残念！もっと小さいです';
 }else{
   message = '0~2の数字を入力してください';
-}
-while(answer < number && number < answer)
+
 
 document.getElementById('choice').textContent = message;
